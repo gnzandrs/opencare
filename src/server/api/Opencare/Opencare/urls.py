c3cb97e29b4f20fceece06519935ef0026e33aab
gnzandrs <gavr@outlook.cl>
2017-03-03 19:39:25 -0300

    neon genesis with django rest framework

--- src/server/api/Opencare/Opencare/urls.py
+++ src/server/api/Opencare/Opencare/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # /index
-    url(r'^', include('main_app.urls'))
+    # url(r'^', include('main_app.urls'))
 ]

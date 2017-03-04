d73106d895354d9fcc3d7ba95fad9d5f53a29342
gnzandrs <gavr@outlook.cl>
2017-03-03 21:13:51 -0300

    person app and test model

--- src/server/api/Opencare/Opencare/urls.py
+++ src/server/api/Opencare/Opencare/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # /index
-    # url(r'^', include('main_app.urls'))
+    # /person
+    # url(r'^', include('person.urls'))
 ]

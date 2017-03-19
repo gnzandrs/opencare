49a9bdc5c9d3d73fd82960125558714d95cbba97
gnzandrs <gavr@outlook.cl>
2017-03-19 01:27:13 -0300

    basic endpoint

--- src/server/api/Opencare/person/urls.py
+++ src/server/api/Opencare/person/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from person import views
 
 urlpatterns = [
-    url(r'^person/$', views.person_list),
-    url(r'^person/(?P<pk>[0-9]+)/$', views.person_detail)
+    url(r'^person/$', views.JSONResponse.person_list),
+    url(r'^person/(?P<pk>[0-9]+)/$', views.JSONResponse.person_detail)
 ]
--- src/server/node/index.js
+++ src/server/node/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var req = require('request');
 
 var logger = require('./logger');
 app.use(logger);
 
 app.get('/', function (request, response) {
-  response.send('Hello World');
+  response.send('Api Index');
+});
+
+app.get('/person/list', function (request, response) {
+  req('http://localhost:8000/person/list', function (error, responseApi, data) {
+    if (!error && responseApi.statusCode == 200) {
+      response.send(data);
+    }
+  });
 });
 
 app.listen(3000, function () {
